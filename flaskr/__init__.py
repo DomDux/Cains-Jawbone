c90ee5c41d196da0c561b2a50ef01062265199cf
DomDux <domjdux@gmail.com>
2024-07-16 00:59:07 +0100

    Test Clean Up: Removed some old tests left over from the Flask tutorial that no longer pass due to my deliberate changes.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -22,12 +22,12 @@ def create_app(test_config=None):
         populate_pages()
 
     # App Config:
-    #  SECRET_KEY is for security/encryption but is 'dev' as a placeholder for now
-    #  DATABASE gives the location of the sqlite database Flask will be running
-    """ app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    ) """
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
 
     # Ensure the instance folder exists
